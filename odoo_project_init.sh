67c11682b9f6f8f5a8adf8a5dfdefc0905949d01
autro21 <heshamrobert21@gmail.com>
2022-01-18 15:46:09 +0200

    bug fixe 12

--- odoo_project_init.sh
+++ odoo_project_init.sh
@@ -25,12 +25,12 @@ do
         pid_file="/opt/odoo$ver/run/$i.pid"
         log_file="/var/log/odoo/$i.log"
         log_level="debug"
+        Curr=`pwd`
         ln /usr/bin/python3.8 /usr/bin/$i
-
         addons_path="/opt/odoo$ver/addons,/opt/odoo$ver/projects/$i/modules"
         sudo ./conf_gen.sh $admin_passwd 5432 $usr $db_passwd $addons_path "/opt/odoo$ver/projects/$i/data" $odoo_port $odoopoll_port $usr 2 1\
-                2147483648 2147483648 8192 3600 3600 ${pid_file} ${log_file} ${log_level} > $i.conf
-        sudo mv $i.conf /etc/odoo/$i/$i.conf
-		sudo ./create_service_odoo.sh $i > $i.service
-        sudo mv $i.service /etc/systemd/system/$i.service
+                2147483648 2147483648 8192 3600 3600 ${pid_file} ${log_file} ${log_level} > $Curr/$i.conf
+        sudo mv $Curr/$i.conf /etc/odoo/$i/$i.conf
+		sudo ./create_service_odoo.sh $i > $Curr/$i.service
+        sudo mv $Curr/$i.service /etc/systemd/system/$i.service
 done;
