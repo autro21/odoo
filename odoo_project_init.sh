369fcdb1f537708b4ff9a02be9e7b690ccfad7c5
autro21 <heshamrobert21@gmail.com>
2022-01-18 15:24:41 +0200

    bug fixe 8

--- odoo_project_init.sh
+++ odoo_project_init.sh
@@ -5,11 +5,11 @@ ver=$OdooVer
 #odoo_lport : string var in jenkins
 for i in "$@";
 do
-		echo $i is made
-		sudo mkdir  /opt/odoo$ver/projects/$i
+		echo "$i is made"
+		sudo mkdir -p /opt/odoo$ver/projects/$i
 		cd /opt/odoo$ver/projects/$i
-		sudo mkdir data
-        sudo mkdir modules
+		sudo mkdir -p data
+        sudo mkdir -p modules
 
         sudo useradd -d /opt/odoo$ver/projects/$i -s /usr/sbin/nologin -G odoo $i
 		sudo chown -R $i:odoo /opt/odoo$ver/projects/$i/
@@ -28,8 +28,8 @@ do
         ln /usr/bin/python3.8 /usr/bin/$i
 
         addons_path="/opt/odoo$ver/addons,/opt/odoo$ver/projects/$i/modules"
-		echo $addons_path
-        sudo ./conf_gen.sh $admin_passwd 5432 $usr $db_passwd $addons_path "/opt/odoo$ver/projects/$i/data" $odoo_port $odoopoll_port $usr 2 1\
-                2147483648 2147483648 8192 3600 3600 ${pid_file} ${log_file} ${log_level} > /etc/odoo/$i/$i.conf
-		sudo ./create_service_odoo.sh $i > /etc/systemd/system/$i.service
+        conf_project=`./conf_gen.sh $admin_passwd 5432 $usr $db_passwd $addons_path "/opt/odoo$ver/projects/$i/data" $odoo_port $odoopoll_port $usr 2 1 2147483648 2147483648 8192 3600 3600 ${pid_file} ${log_file} ${log_level}`
+        sudo echo $conf_project > /etc/odoo/$i/$i.conf
+        serv_conf=`./create_service_odoo.sh $i`
+		sudo echo $serv_conf > /etc/systemd/system/$i.service
 done;
