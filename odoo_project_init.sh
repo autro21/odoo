cc24e7f95470fbf341953fc26beff4584fff527b
autro21 <heshamrobert21@gmail.com>
2022-01-18 15:08:26 +0200

    bug fixe 7

--- odoo_project_init.sh
+++ odoo_project_init.sh
@@ -31,5 +31,5 @@ do
 		echo $addons_path
         sudo ./conf_gen.sh $admin_passwd 5432 $usr $db_passwd $addons_path "/opt/odoo$ver/projects/$i/data" $odoo_port $odoopoll_port $usr 2 1\
                 2147483648 2147483648 8192 3600 3600 ${pid_file} ${log_file} ${log_level} > /etc/odoo/$i/$i.conf
-		sudo ./create_service_odoo.sh $i > /etc/systemd/system/$i.service		
+		sudo ./create_service_odoo.sh $i > /etc/systemd/system/$i.service
 done;
